1aad89dc08eadaa32a3d00ca229d2aeac1292593
Smriti Yadav <smritiyadav.dev@gmail.com>
2026-02-27 02:05:05 +0530

    Updated rbac

--- helpers/rbac_helper.py
+++ helpers/rbac_helper.py
@@ -1,6 +1,5 @@
 from functools import wraps
-from fastapi import status
-from helpers.response_parser import generate_response
+from fastapi import status, HTTPException
 from enums.enum_helper import UserRole
 
 
@@ -8,21 +7,19 @@ def role_required(allowed_roles: list[UserRole]):
     def decorator(func):
         @wraps(func)
         async def wrapper(*args, **kwargs):
-
-            current_user = kwargs.get("current_user")
-
+            
+            current_user = kwargs.get('current_user')
+            
             if not current_user:
-                return generate_response(
+                raise HTTPException(
                     status_code=status.HTTP_401_UNAUTHORIZED,
-                    message="Authentication required",
-                    success=False,
+                    detail="Authentication required",
                 )
 
             if current_user.role not in allowed_roles:
-                return generate_response(
+                raise HTTPException(
                     status_code=status.HTTP_403_FORBIDDEN,
-                    message="You do not have permission to perform this action",
-                    success=False,
+                    detail="You do not have permission to perform this action",
                 )
 
             return await func(*args, **kwargs)
